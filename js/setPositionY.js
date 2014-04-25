7ca4e02f10f38851452472d406a7c84f3cfe91c0
aikawa <xxx@iMac-3.local>
2014-04-26 04:35:37 +0900

    html

---
+++ js/setPositionY.js
@@ -0,0 +1,47 @@
+$(function() {
+    var top = $('.content').offset().left; //y座標の初期値を取得
+		
+		
+		
+		
+    $(window).scroll(function() {
+        var value = $(this).scrollTop(); //スクロール値を取得
+        $('#scrollValue').text(value);
+
+        //取得したスクロール値を矩形のY座標値に指定
+        //スクロール値を除算して移動距離に変化をつける
+        $('.content').css('top', left + value / 2);
+        $('.content').css('top', left + value / 4);
+        $('.content').css('top', left + value / 6);
+        $('.content').css('top', top + value / 8);
+        $('.content').css('top', top + value / 10);
+        });
+});
+
+
+
+/*  
+
+$(function() {
+	var top = $('.content').offset().top; //y座標の初期値を取得
+	var left1 = $('#item1').offset().left; //item1のx座標の初期値
+	var left2 = $('#item2').offset().left; //item2のx座標の初期値
+	var left3 = $('#item3').offset().left; //item3のx座標の初期値
+	var left4 = $('#item4').offset().left; //item4のx座標の初期値
+	var left5 = $('#item5').offset().left; //item5のx座標の初期値
+
+	$(window).scroll(function() {
+		var value = $(this).scrollTop(); //スクロール値を取得
+		$('#scrollValue').text(value);
+
+		//取得したスクロール量を矩形のXY座標値に指定
+		//スクロール値を除算して移動距離に変化をつける
+		$('#item1').css({'top': top + value / 4});
+		$('#item2').css({'top': top + value / 4 * Math.sin(57.5 / 180 * Math.PI), 'left': left2 + value / 4 * Math.cos(57.5 / 180 * Math.PI)});
+		$('#item3').css({'top': top + value / 4 * Math.sin(45 / 180 * Math.PI), 'left': left3 + value / 4 * Math.cos(45 / 180 * Math.PI)});
+		$('#item4').css({'top': top + value / 4 * Math.sin(12.5 / 180 * Math.PI), 'left': left4 + value / 4 * Math.cos(12.5 / 180 * Math.PI)});
+		$('#item5').css({'left': left5 + value / 4});
+	});
+});
+
+*/
